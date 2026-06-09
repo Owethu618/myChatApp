8142693bbb2bbbadae98658401cce7b6263f55c3
Owethu618 <ST10518932@rcconnect.edu.za>
2026-06-09 10:10:56 +0200

    Add imports that are required

--- src/main/java/com/mycompany/mychatapp/Message.java
+++ src/main/java/com/mycompany/mychatapp/Message.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.mychatapp;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 import java.io.FileWriter;
 import java.io.IOException;
